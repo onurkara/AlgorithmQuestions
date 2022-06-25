cfc240387ba21e537dfb30853425373eaf7d7d14
Onur Kara <onurkara92@gmail.com>
2022-06-25 14:34:04 +0300

    Update removeDuplicates solution

--- Arrays.playground/Pages/RemoveDuplicates.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/RemoveDuplicates.xcplaygroundpage/Contents.swift
@@ -1,15 +1,18 @@
 import XCTest
 
 func removeDuplicates(_ nums: inout [Int]) -> Int {
-    var index = 0
-    while index < nums.endIndex-1 {
-        if nums[index] == nums[index+1] {
-            nums.remove(at: index)
-        } else {
-            index += 1
-        }
+    var initialUniqueItemIndex = 0
+
+    if nums.isEmpty {
+        return 0
     }
 
+    for index in 0..<nums.count - 1 {
+        if nums[index] != nums[index + 1 ] {
+            initialUniqueItemIndex += 1
+            nums[initialUniqueItemIndex] = nums[index+1]
+        }
+    }
     return nums.count
 }
 
@@ -17,44 +20,39 @@ final class RemoveDuplicateTests: XCTestCase {
 
     func testRemoveDuplicatesEmptyArray() {
         var nums: [Int] = []
-        let newCounts = removeDuplicates(&nums)
-        XCTAssertEqual(newCounts, 0)
+        removeDuplicates(&nums)
         XCTAssertEqual(nums, [])
     }
 
     func testRemoveDuplicatesSingleItemArray() {
         var nums: [Int] = [1]
-        let newCounts = removeDuplicates(&nums)
-        XCTAssertEqual(newCounts, 1)
+        removeDuplicates(&nums)
         XCTAssertEqual(nums, [1])
     }
 
     func testRemoveDuplicatesUniqueArray() {
         var nums: [Int] = [1,2]
-        let newCounts = removeDuplicates(&nums)
-        XCTAssertEqual(newCounts, 2)
+        removeDuplicates(&nums)
         XCTAssertEqual(nums, [1,2])
     }
 
     func testRemoveDuplicates1() {
         var nums = [1,1,2]
-        let newCounts = removeDuplicates(&nums)
-        XCTAssertEqual(newCounts, 2)
-        XCTAssertEqual(nums, [1,2])
+        removeDuplicates(&nums)
+        XCTAssertEqual(nums, [1,2,2])
     }
+
 
     func testRemoveDuplicates2() {
         var nums = [1,1,2,2]
-        let newCounts = removeDuplicates(&nums)
-        XCTAssertEqual(newCounts, 2)
-        XCTAssertEqual(nums, [1,2])
+        removeDuplicates(&nums)
+        XCTAssertEqual(nums, [1,2,2,2])
     }
 
     func testRemoveDuplicates3() {
         var nums = [1,1,2,2,3,4,5,5,5,5]
-        let newCounts = removeDuplicates(&nums)
-        XCTAssertEqual(newCounts, 5)
-        XCTAssertEqual(nums, [1,2,3,4,5])
+        removeDuplicates(&nums)
+        XCTAssertEqual(nums, [1,2,3,4,5,4,5,5,5,5])
     }
 }
 
